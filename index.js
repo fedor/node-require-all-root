12905fb56993dcc2356b3f10d0ee29ae7673d1ac
Fedor Korshunov <mail@fedor.cc>
2016-08-17 00:58:16 +0800

    enabled universal usage

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var requireAll = require('require-all')
 var rootPath = require('app-root-path')
+
 module.exports = function(path) {
-	return requireAll(rootPath + path)
+	var fullPath = rootPath + path
+	
+	try {
+		return require(fullPath)
+	} catch (e) {
+		// No fullPath.js, fullPath/index.js or fullPath/index.node
+	}
+
+	try {
+		return requireAll(fullPath)
+	} catch (e) {
+		return require(path)
+	}
 }
