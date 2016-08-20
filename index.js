8fad7d7daa62a08038be2019ebdbdf8222197123
Fedor Korshunov <mail@fedor.cc>
2016-08-20 19:08:50 +0800

    Ensure module inner errors are thrown

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ module.exports = function(path) {
 	try {
 		return require(fullPath)
 	} catch (e) {
+		if (e.code !== 'MODULE_NOT_FOUND') {
+			throw e
+		}
 		// No fullPath.js, fullPath/index.js or fullPath/index.node
 	}
 
 	try {
 		return requireAll(fullPath)
 	} catch (e) {
+		if (e.code !== 'ENOENT') {
+			throw e
+		}
+		// No full path
 		return require(path)
 	}
 }
